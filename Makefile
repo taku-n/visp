17bb9c52dab0047a5a161e0cc5bbc549b936243c
Takuto Nanjo <16991946+taku-n@users.noreply.github.com>
2019-11-03 23:26:29 +0900

    optimize for Windows

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 all: visp.exe
 
-visp.exe:
-	GOOS=windows GOARCH=amd64 go build -o visp.exe main.go
+visp.exe: main.go
+	GOOS=windows GOARCH=amd64 go build -o visp.exe -ldflags="-H windowsgui" main.go
+
+rm:
+	rm visp.exe
--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import "fmt"
 import "net/http"
-
-const CONTENTS = `hello, world
-`
+import "os/exec"
 
 type DispViewHandler struct{}
 
@@ -15,5 +13,42 @@ func (h *DispViewHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var handler DispViewHandler = DispViewHandler{}
 
-	http.ListenAndServe("127.0.0.1:8080", &handler)
+	go http.ListenAndServe("127.0.0.1:8080", &handler)
+
+	// Edit the path to run your Google Chrome
+	exec.Command("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+			"--new-window",
+			"http://127.0.0.1:8080/").Run()
 }
+
+const CONTENTS = `<head>
+<title>visp</title>
+</head>
+
+<body>
+<h1>visp</h1>
+<video autoplay playsinline width="960" height="540"></video>
+
+<script>
+const mediaStreamConstraints = {
+	video: true
+};
+
+const localVideo = document.querySelector("video");
+
+function gotLocalMediaStream(mediaStream) {
+	const localStream = mediaStream;
+	localVideo.srcObject = mediaStream;
+}
+
+function handleLocalMediaStreamError(error) {
+	console.log("navigator.getUserMedia error: ", error);
+}
+
+navigator.mediaDevices
+		.getDisplayMedia(mediaStreamConstraints)
+		.then(gotLocalMediaStream)
+		.catch(handleLocalMediaStreamError);
+</script>
+</body>
+`
